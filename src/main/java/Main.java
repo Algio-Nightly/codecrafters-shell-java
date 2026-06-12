3b6f671de1b1a3f05a3799e2b56383df3420091a
Algio <algorithmic1452@gmail.com>
2026-06-12 12:30:33 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         // Uncomment the code below to pass the first stage
+        Scanner sc = new Scanner(System.in);
         System.out.print("$ ");
+        String command = sc.next();
+        System.out.println(command+": command not found");
     }
 }
