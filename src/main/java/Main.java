b9514345848ef33e4213531b94a8a217d7442936
Algio <algorithmic1452@gmail.com>
2026-06-13 06:20:34 +0000

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,12 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Main {
+
+    // static String[] tokenizer(){
+
+    //     return new String[5];
+    // }
     public static void main(String[] args) throws Exception {
         // Uncomment the code below to pass the first stage
         Scanner sc = new Scanner(System.in);
@@ -9,12 +15,21 @@ public class Main {
 
             System.out.print("$ ");
             String command = sc.nextLine();
-            String[] commands = command.split(" ");
-            if (commands[0].equals("exit")){
+            ArrayList<String> commands = new ArrayList<>(Arrays.asList(command.split(" ")));
+            if (commands.get(0).equals("exit")){
                 break;
             }
-            System.out.println(commands[0]+": command not found");
+            else if (commands.get(0).equals("echo")){
+                echo(commands);
+            } else{
+                System.out.println(commands.get(0)+": command not found");
+            }
         }
         sc.close();
+    }
+
+    static void echo(ArrayList<String> command){
+        command.remove(0);
+        System.out.println(String.join(" ", command));
     }
 }
